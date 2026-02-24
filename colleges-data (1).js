c0c09fdcdbc67b58768ae19c5e3d71a1481a28b8
Anuj Jha <anujjha2309@gmail.com>
2026-02-24 19:18:18 +0530

    Add files via upload

---
+++ colleges-data (1).js
@@ -0,0 +1,21 @@
+
+const collegesData = [
+{slug:"amity-mba",name:"Amity University Online",program:"MBA",fees:199000,duration:"2 Years",accreditation:"NAAC A+"},
+{slug:"nmims-mba",name:"NMIMS Global Access",program:"MBA",fees:196000,duration:"2 Years",accreditation:"NAAC A+"},
+{slug:"manipal-mba",name:"Manipal University Online",program:"MBA",fees:292000,duration:"2 Years",accreditation:"NAAC A+"},
+{slug:"lpu-mba",name:"LPU Online MBA",program:"MBA",fees:180000,duration:"2 Years",accreditation:"NAAC A++"},
+{slug:"jain-mba",name:"Jain University Online",program:"MBA",fees:175000,duration:"2 Years",accreditation:"NAAC A+"},
+
+{slug:"lpu-bba",name:"LPU Online BBA",program:"BBA",fees:120000,duration:"3 Years",accreditation:"NAAC A++"},
+{slug:"cu-bba",name:"Chandigarh University BBA",program:"BBA",fees:140000,duration:"3 Years",accreditation:"NAAC A+"},
+{slug:"amity-bba",name:"Amity Online BBA",program:"BBA",fees:165000,duration:"3 Years",accreditation:"NAAC A+"},
+
+{slug:"jain-bca",name:"Jain University BCA",program:"BCA",fees:150000,duration:"3 Years",accreditation:"NAAC A+"},
+{slug:"amrita-bca",name:"Amrita Online BCA",program:"BCA",fees:120000,duration:"3 Years",accreditation:"NAAC A++"},
+{slug:"lpu-bca",name:"LPU Online BCA",program:"BCA",fees:135000,duration:"3 Years",accreditation:"NAAC A++"},
+
+{slug:"ignou-mca",name:"IGNOU MCA",program:"MCA",fees:62000,duration:"2 Years",accreditation:"Central University"},
+{slug:"sikkim-mca",name:"Sikkim Manipal MCA",program:"MCA",fees:125000,duration:"2 Years",accreditation:"NAAC A+"},
+{slug:"cu-mca",name:"Chandigarh University MCA",program:"MCA",fees:150000,duration:"2 Years",accreditation:"NAAC A+"},
+{slug:"lpu-mca",name:"LPU Online MCA",program:"MCA",fees:160000,duration:"2 Years",accreditation:"NAAC A++"}
+];
